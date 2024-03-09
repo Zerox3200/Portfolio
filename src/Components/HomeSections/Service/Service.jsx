aac1ec00d4623ac3d968287b8a90bb6f03a3259c
Zerox3200 <ziad.ahmed01020338066@gmail.com>
2024-03-09 21:09:06 +0200

    Emerg

--- src/Components/HomeSections/Service/Service.jsx
+++ src/Components/HomeSections/Service/Service.jsx
@@ -5,7 +5,7 @@ import ServiceType from './Services/ServiceType';
 export default function Service() {
     return (
         <section className='Service container' id='Service'>
-            <h1 className='h3'>Service</h1>
+            <h1 className='h3 Service_title'>Service</h1>
             <p>Whether you need a sleek portfolio website, an engaging web application,
                 or customized web development services tailored to your unique requirements,
                 I'm here to turn your vision into reality. Let's collaborate and bring your ideas to real life!
